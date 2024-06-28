29d0b9e6c18c1b9430593a959037171529095ddd
Turdugul <okonbaevaturdugul@gmail.com>
2024-06-28 16:04:27 +0100

    first commit

---
+++ src/components/Question.js
@@ -0,0 +1,12 @@
+import Option from "./Option";
+
+function Question({ question, answer, dispatch }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Option question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
+
+export default Question;
